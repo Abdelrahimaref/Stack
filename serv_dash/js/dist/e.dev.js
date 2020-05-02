f9b5ea1726cd473cd25a08636353d3b25adcfe67
abdelrahim <40636672+Abdelrahimaref@users.noreply.github.com>
2020-05-02 18:34:33 +0200

    gg

--- serv_dash/js/dist/e.dev.js
+++ serv_dash/js/dist/e.dev.js
@@ -1,29 +1,55 @@
 "use strict";
 
 $('.send-req-btn').click(function () {
-  $('.send-Req').show(350);
   $('.track-pay').hide(350);
   $('.incoming-req').hide(350);
+  $('.contract-requests').hide(350);
+  $('.send-Req').show(350);
   $(this).addClass("profile-nav__active");
   $('.all-req-btn').removeClass("profile-nav__active");
   $('.track-pay-btn').removeClass("profile-nav__active");
+  $('.contract-btn').removeClass("profile-nav__active");
 });
 $('.all-req-btn').click(function () {
   $('.send-Req').hide(350);
   $('.track-pay').hide(350);
+  $('.contract-requests ').hide(350);
   $('.incoming-req').show(350);
   $(this).addClass("profile-nav__active");
   $('.send-req-btn').removeClass("profile-nav__active");
   $('.track-pay-btn').removeClass("profile-nav__active");
+  $('.contract-btn').removeClass("profile-nav__active");
 });
 $('.track-pay-btn').click(function () {
   $('.send-Req').hide(350);
-  $('.track-pay').show(350);
   $('.incoming-req').hide(350);
+  $('.contract-requests ').hide(350);
+  $('.track-pay').show(350);
   $(this).addClass("profile-nav__active");
   $('.send-req-btn').removeClass("profile-nav__active");
   $('.all-req-btn').removeClass("profile-nav__active");
+  $('.contract-btn').removeClass("profile-nav__active");
 });
+$('.contract-btn').click(function () {
+  $('.send-Req').hide(350);
+  $('.incoming-req').hide(350);
+  $('.track-pay').hide(350);
+  $('.contract-requests ').show(350);
+  $(this).addClass("profile-nav__active");
+  $('.send-req-btn').removeClass("profile-nav__active");
+  $('.all-req-btn').removeClass("profile-nav__active");
+  $('.track-pay-btn').removeClass("profile-nav__active");
+});
+var sel = document.getElementById('sel'),
+    hide = document.getElementById('hide');
+
+sel.onchange = function () {
+  if (this.value === 'Directly_customer_support') {
+    hide.style.display = 'block';
+  } else {
+    hide.style.display = 'none';
+  }
+};
 
 function success() {
   var Toast = Swal.mixin({
@@ -35,6 +61,45 @@ function success() {
   Toast.fire({
     type: 'success',
     title: 'Request Send successfully'
+  });
+}
+
+function accept() {
+  var Toast = Swal.mixin({
+    toast: false,
+    position: 'center',
+    showConfirmButton: false,
+    timer: 3500
+  });
+  Toast.fire({
+    type: 'success',
+    title: 'Request Accepted successfully'
+  });
+}
+
+function Canceled() {
+  var Toast = Swal.mixin({
+    toast: false,
+    position: 'center',
+    showConfirmButton: false,
+    timer: 3500
+  });
+  Toast.fire({
+    type: 'success',
+    title: 'Request Refused successfully'
+  });
+}
+
+function complain() {
+  var Toast = Swal.mixin({
+    toast: false,
+    position: 'center',
+    showConfirmButton: false,
+    timer: 3500
+  });
+  Toast.fire({
+    type: 'success',
+    title: 'Compain Sent successfully'
   });
 }
 
--- serv_dash/js/e.js
+++ serv_dash/js/e.js
@@ -1,28 +1,57 @@
 
 $('.send-req-btn').click(function(){
-	$('.send-Req').show(350);
 	$('.track-pay').hide(350);
-	$('.incoming-req').hide(350);
+  $('.incoming-req').hide(350);
+  $('.contract-requests').hide(350);
+  $('.send-Req').show(350);
 	$(this).addClass("profile-nav__active");
 	$('.all-req-btn').removeClass("profile-nav__active");
-	$('.track-pay-btn').removeClass("profile-nav__active");
+  $('.track-pay-btn').removeClass("profile-nav__active");
+  $('.contract-btn').removeClass("profile-nav__active")
 });
 $('.all-req-btn').click(function(){
 	$('.send-Req').hide(350);
-	$('.track-pay').hide(350);
+  $('.track-pay').hide(350);
+  $('.contract-requests ').hide(350);
 	$('.incoming-req').show(350);
 	$(this).addClass("profile-nav__active");
 	$('.send-req-btn').removeClass("profile-nav__active");
-	$('.track-pay-btn').removeClass("profile-nav__active");
+  $('.track-pay-btn').removeClass("profile-nav__active");
+  $('.contract-btn').removeClass("profile-nav__active")
 });
 $('.track-pay-btn').click(function(){
 	$('.send-Req').hide(350);
-	$('.track-pay').show(350);
-	$('.incoming-req').hide(350);
+  $('.incoming-req').hide(350);
+  $('.contract-requests ').hide(350);
+  $('.track-pay').show(350);
 	$(this).addClass("profile-nav__active");
 	$('.send-req-btn').removeClass("profile-nav__active");
-	$('.all-req-btn').removeClass("profile-nav__active");
+  $('.all-req-btn').removeClass("profile-nav__active");
+  $('.contract-btn').removeClass("profile-nav__active")
 });
+$('.contract-btn').click(function(){
+	$('.send-Req').hide(350);
+  $('.incoming-req').hide(350);
+  $('.track-pay').hide(350);
+  $('.contract-requests ').show(350);
+	$(this).addClass("profile-nav__active");
+	$('.send-req-btn').removeClass("profile-nav__active");
+  $('.all-req-btn').removeClass("profile-nav__active");
+  $('.track-pay-btn').removeClass("profile-nav__active")
+});
+
+var sel    =  document.getElementById('sel'),
+hide   = document.getElementById('hide');
+sel.onchange = function(){
+  if(this.value==='Directly_customer_support')
+  {
+    hide.style.display='block';
+  }
+  else
+  {
+    hide.style.display='none';
+  }
+}
 
 function success() {
     const Toast = Swal.mixin({
@@ -31,12 +60,52 @@ function success() {
         showConfirmButton: false,
         timer: 3500
       })
-      
       Toast.fire({
         type: 'success',
         title: 'Request Send successfully'
       })
 }
+
+function accept() {
+  const Toast = Swal.mixin({
+      toast: false,
+      position: 'center',
+      showConfirmButton: false,
+      timer: 3500
+    })
+    
+    Toast.fire({
+      type: 'success',
+      title: 'Request Accepted successfully'
+    })
+}
+function Canceled() {
+  const Toast = Swal.mixin({
+      toast: false,
+      position: 'center',
+      showConfirmButton: false,
+      timer: 3500
+    })
+    
+    Toast.fire({
+      type: 'success',
+      title: 'Request Refused successfully'
+    })
+}
+function complain() {
+  const Toast = Swal.mixin({
+      toast: false,
+      position: 'center',
+      showConfirmButton: false,
+      timer: 3500
+    })
+    
+    Toast.fire({
+      type: 'success',
+      title: 'Compain Sent successfully'
+    })
+}
+
 function success2() {
     const Toast = Swal.mixin({
         toast: false,
